7cba5862859c701b857c644058266eac6e7a13f3
DragonSCRIPTS <cleyltondragonscp@gmail.com>
2025-04-20 22:33:49 -0300

    Create World3.lua

---
+++ World3.lua
@@ -0,0 +1,539 @@
+local World3 = {}
+
+-- Estrutura moderna para quests do Mundo 3 (2025)
+World3.Quests = {
+    -- Level 1500-1524
+    {
+        Level = {Min = 1500, Max = 1524},
+        Quest = {
+            Name = "PiratePortQuest",
+            Level = 1,
+            Title = "Pirate Port Quest"
+        },
+        Monster = {
+            Name = "Pirate Millionaire",
+            SpawnPoint = CFrame.new(-245.9963836669922, 47.30615234375, 5584.1005859375)
+        },
+        QuestPoint = CFrame.new(-290.074677, 42.9034653, 5581.58984, 0.965929627, -0, -0.258804798, 0, 1, -0, 0.258804798, 0, 0.965929627)
+    },
+    -- Level 1525-1574
+    {
+        Level = {Min = 1525, Max = 1574},
+        Quest = {
+            Name = "PiratePortQuest",
+            Level = 2,
+            Title = "Pirate Port Quest"
+        },
+        Monster = {
+            Name = "Pistol Billionaire",
+            SpawnPoint = CFrame.new(-187.3301544189453, 86.23987579345703, 6013.513671875)
+        },
+        QuestPoint = CFrame.new(-290.074677, 42.9034653, 5581.58984, 0.965929627, -0, -0.258804798, 0, 1, -0, 0.258804798, 0, 0.965929627)
+    },
+    -- Level 1575-1599
+    {
+        Level = {Min = 1575, Max = 1599},
+        Quest = {
+            Name = "AmazonQuest",
+            Level = 1,
+            Title = "Amazon Quest"
+        },
+        Monster = {
+            Name = "Dragon Crew Warrior",
+            SpawnPoint = CFrame.new(6141.140625, 51.35136413574219, -1340.738525390625)
+        },
+        QuestPoint = CFrame.new(5832.83594, 51.6806107, -1101.51563, 0.898790359, -0, -0.438378751, 0, 1, -0, 0.438378751, 0, 0.898790359)
+    },
+    -- Level 1600-1624
+    {
+        Level = {Min = 1600, Max = 1624},
+        Quest = {
+            Name = "AmazonQuest",
+            Level = 2,
+            Title = "Amazon Quest"
+        },
+        Monster = {
+            Name = "Dragon Crew Archer",
+            SpawnPoint = CFrame.new(6616.41748046875, 441.7670593261719, 446.0469970703125)
+        },
+        QuestPoint = CFrame.new(5833.1147460938, 51.60498046875, -1103.0693359375)
+    },
+    -- Level 1625-1649
+    {
+        Level = {Min = 1625, Max = 1649},
+        Quest = {
+            Name = "AmazonQuest2",
+            Level = 1,
+            Title = "Amazon Quest 2"
+        },
+        Monster = {
+            Name = "Female Islander",
+            SpawnPoint = CFrame.new(4685.25830078125, 735.8078002929688, 815.3425903320312)
+        },
+        QuestPoint = CFrame.new(5446.8793945313, 601.62945556641, 749.45672607422)
+    },
+    -- Level 1650-1699
+    {
+        Level = {Min = 1650, Max = 1699},
+        Quest = {
+            Name = "AmazonQuest2",
+            Level = 2,
+            Title = "Amazon Quest 2"
+        },
+        Monster = {
+            Name = "Giant Islander",
+            SpawnPoint = CFrame.new(4729.09423828125, 590.436767578125, -36.97627639770508)
+        },
+        QuestPoint = CFrame.new(5446.8793945313, 601.62945556641, 749.45672607422)
+    },
+    -- Level 1700-1724
+    {
+        Level = {Min = 1700, Max = 1724},
+        Quest = {
+            Name = "MarineTreeIsland",
+            Level = 1,
+            Title = "Marine Tree Island"
+        },
+        Monster = {
+            Name = "Marine Commodore",
+            SpawnPoint = CFrame.new(2286.0078125, 73.13391876220703, -7159.80908203125)
+        },
+        QuestPoint = CFrame.new(2180.54126, 27.8156815, -6741.5498, -0.965929747, 0, 0.258804798, 0, 1, 0, -0.258804798, 0, -0.965929747)
+    },
+    -- Level 1725-1774
+    {
+        Level = {Min = 1725, Max = 1774},
+        Quest = {
+            Name = "MarineTreeIsland",
+            Level = 2,
+            Title = "Marine Tree Island"
+        },
+        Monster = {
+            Name = "Marine Rear Admiral",
+            SpawnPoint = CFrame.new(3656.773681640625, 160.52406311035156, -7001.5986328125)
+        },
+        QuestPoint = CFrame.new(2179.98828125, 28.731239318848, -6740.0551757813)
+    },
+    -- Level 1775-1799
+    {
+        Level = {Min = 1775, Max = 1799},
+        Quest = {
+            Name = "DeepForestIsland3",
+            Level = 1,
+            Title = "Deep Forest Island 3"
+        },
+        Monster = {
+            Name = "Fishman Raider",
+            SpawnPoint = CFrame.new(-10407.5263671875, 331.76263427734375, -8368.5166015625)
+        },
+        QuestPoint = CFrame.new(-10581.6563, 330.872955, -8761.18652, -0.882952213, 0, 0.469463557, 0, 1, 0, -0.469463557, 0, -0.882952213)
+    },
+    -- Level 1800-1824
+    {
+        Level = {Min = 1800, Max = 1824},
+        Quest = {
+            Name = "DeepForestIsland3",
+            Level = 2,
+            Title = "Deep Forest Island 3"
+        },
+        Monster = {
+            Name = "Fishman Captain",
+            SpawnPoint = CFrame.new(-10994.701171875, 352.38140869140625, -9002.1103515625)
+        },
+        QuestPoint = CFrame.new(-10581.6563, 330.872955, -8761.18652, -0.882952213, 0, 0.469463557, 0, 1, 0, -0.469463557, 0, -0.882952213)
+    },
+    -- Level 1825-1849
+    {
+        Level = {Min = 1825, Max = 1849},
+        Quest = {
+            Name = "DeepForestIsland",
+            Level = 1,
+            Title = "Deep Forest Island"
+        },
+        Monster = {
+            Name = "Forest Pirate",
+            SpawnPoint = CFrame.new(-13274.478515625, 332.3781433105469, -7769.58056640625)
+        },
+        QuestPoint = CFrame.new(-13234.04, 331.488495, -7625.40137, 0.707134247, -0, -0.707079291, 0, 1, -0, 0.707079291, 0, 0.707134247)
+    },
+    -- Level 1850-1899
+    {
+        Level = {Min = 1850, Max = 1899},
+        Quest = {
+            Name = "DeepForestIsland",
+            Level = 2,
+            Title = "Deep Forest Island"
+        },
+        Monster = {
+            Name = "Mythological Pirate",
+            SpawnPoint = CFrame.new(-13680.607421875, 501.08154296875, -6991.189453125)
+        },
+        QuestPoint = CFrame.new(-13234.04, 331.488495, -7625.40137, 0.707134247, -0, -0.707079291, 0, 1, -0, 0.707079291, 0, 0.707134247)
+    },
+    -- Level 1900-1924
+    {
+        Level = {Min = 1900, Max = 1924},
+        Quest = {
+            Name = "DeepForestIsland2",
+            Level = 1,
+            Title = "Deep Forest Island 2"
+        },
+        Monster = {
+            Name = "Jungle Pirate",
+            SpawnPoint = CFrame.new(-12256.16015625, 331.73828125, -10485.8369140625)
+        },
+        QuestPoint = CFrame.new(-12680.3818, 389.971039, -9902.01953, -0.0871315002, 0, 0.996196866, 0, 1, 0, -0.996196866, 0, -0.0871315002)
+    },
+    -- Level 1925-1974
+    {
+        Level = {Min = 1925, Max = 1974},
+        Quest = {
+            Name = "DeepForestIsland2",
+            Level = 2,
+            Title = "Deep Forest Island 2"
+        },
+        Monster = {
+            Name = "Musketeer Pirate",
+            SpawnPoint = CFrame.new(-13457.904296875, 391.545654296875, -9859.177734375)
+        },
+        QuestPoint = CFrame.new(-12680.3818, 389.971039, -9902.01953, -0.0871315002, 0, 0.996196866, 0, 1, 0, -0.996196866, 0, -0.0871315002)
+    },
+    -- Level 1975-1999
+    {
+        Level = {Min = 1975, Max = 1999},
+        Quest = {
+            Name = "HauntedQuest1",
+            Level = 1,
+            Title = "Haunted Quest"
+        },
+        Monster = {
+            Name = "Reborn Skeleton",
+            SpawnPoint = CFrame.new(-8763.7236328125, 165.72299194335938, 6159.86181640625)
+        },
+        QuestPoint = CFrame.new(-9479.2168, 141.215088, 5566.09277, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+    },
+    -- Level 2000-2024
+    {
+        Level = {Min = 2000, Max = 2024},
+        Quest = {
+            Name = "HauntedQuest1",
+            Level = 2,
+            Title = "Haunted Quest"
+        },
+        Monster = {
+            Name = "Living Zombie",
+            SpawnPoint = CFrame.new(-10144.1318359375, 138.62667846679688, 5838.0888671875)
+        },
+        QuestPoint = CFrame.new(-9479.2168, 141.215088, 5566.09277, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+    },
+    -- Level 2025-2049
+    {
+        Level = {Min = 2025, Max = 2049},
+        Quest = {
+            Name = "HauntedQuest2",
+            Level = 1,
+            Title = "Haunted Quest 2"
+        },
+        Monster = {
+            Name = "Demonic Soul",
+            SpawnPoint = CFrame.new(-9505.8720703125, 172.10482788085938, 6158.9931640625)
+        },
+        QuestPoint = CFrame.new(-9516.99316, 172.017181, 6078.46533, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+    },
+    -- Level 2050-2074
+    {
+        Level = {Min = 2050, Max = 2074},
+        Quest = {
+            Name = "HauntedQuest2",
+            Level = 2,
+            Title = "Haunted Quest 2"
+        },
+        Monster = {
+            Name = "Posessed Mummy",
+            SpawnPoint = CFrame.new(-9582.0224609375, 6.251527309417725, 6205.478515625)
+        },
+        QuestPoint = CFrame.new(-9516.99316, 172.017181, 6078.46533, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+    },
+    -- Level 2075-2099
+    {
+        Level = {Min = 2075, Max = 2099},
+        Quest = {
+            Name = "NutsIslandQuest",
+            Level = 1,
+            Title = "Nuts Island Quest"
+        },
+        Monster = {
+            Name = "Peanut Scout",
+            SpawnPoint = CFrame.new(-2143.241943359375, 47.72198486328125, -10029.9951171875)
+        },
+        QuestPoint = CFrame.new(-2104.3908691406, 38.104167938232, -10194.21875, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+    },
+    -- Level 2100-2124
+    {
+        Level = {Min = 2100, Max = 2124},
+        Quest = {
+            Name = "NutsIslandQuest",
+            Level = 2,
+            Title = "Nuts Island Quest"
+        },
+        Monster = {
+            Name = "Peanut President",
+            SpawnPoint = CFrame.new(-1859.35400390625, 38.10316848754883, -10422.4296875)
+        },
+        QuestPoint = CFrame.new(-2104.3908691406, 38.104167938232, -10194.21875, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+    },
+    -- Level 2125-2149
+    {
+        Level = {Min = 2125, Max = 2149},
+        Quest = {
+            Name = "IceCreamIslandQuest",
+            Level = 1,
+            Title = "Ice Cream Island Quest"
+        },
+        Monster = {
+            Name = "Ice Cream Chef",
+            SpawnPoint = CFrame.new(-872.24658203125, 65.81957244873047, -10919.95703125)
+        },
+        QuestPoint = CFrame.new(-820.64825439453, 65.819526672363, -10965.795898438, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+    },
+    -- Level 2150-2199
+    {
+        Level = {Min = 2150, Max = 2199},
+        Quest = {
+            Name = "IceCreamIslandQuest",
+            Level = 2,
+            Title = "Ice Cream Island Quest"
+        },
+        Monster = {
+            Name = "Ice Cream Commander",
+            SpawnPoint = CFrame.new(-558.06103515625, 112.04895782470703, -11290.7744140625)
+        },
+        QuestPoint = CFrame.new(-820.64825439453, 65.819526672363, -10965.795898438, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+    },
+    -- Level 2200-2224
+    {
+        Level = {Min = 2200, Max = 2224},
+        Quest = {
+            Name = "CakeQuest1",
+            Level = 1,
+            Title = "Cake Quest"
+        },
+        Monster = {
+            Name = "Cookie Crafter",
+            SpawnPoint = CFrame.new(-2374.13671875, 37.79826354980469, -12125.30859375)
+        },
+        QuestPoint = CFrame.new(-2021.32007, 37.7982254, -12028.7295, 0.957576931, -8.80302053e-08, 0.288177818, 6.9301187e-08, 1, 7.51931211e-08, -0.288177818, -5.2032135e-08, 0.957576931)
+    },
+    -- Level 2225-2249
+    {
+        Level = {Min = 2225, Max = 2249},
+        Quest = {
+            Name = "CakeQuest1",
+            Level = 2,
+            Title = "Cake Quest"
+        },
+        Monster = {
+            Name = "Cake Guard",
+            SpawnPoint = CFrame.new(-1598.3070068359375, 43.773197174072266, -12244.5810546875)
+        },
+        QuestPoint = CFrame.new(-2021.32007, 37.7982254, -12028.7295, 0.957576931, -8.80302053e-08, 0.288177818, 6.9301187e-08, 1, 7.51931211e-08, -0.288177818, -5.2032135e-08, 0.957576931)
+    },
+    -- Level 2250-2274
+    {
+        Level = {Min = 2250, Max = 2274},
+        Quest = {
+            Name = "CakeQuest2",
+            Level = 1,
+            Title = "Cake Quest 2"
+        },
+        Monster = {
+            Name = "Baking Staff",
+            SpawnPoint = CFrame.new(-1887.8099365234375, 77.6185073852539, -12998.3505859375)
+        },
+        QuestPoint = CFrame.new(-1927.91602, 37.7981339, -12842.5391, -0.96804446, 4.22142143e-08, 0.250778586, 4.74911062e-08, 1, 1.49904711e-08, -0.250778586, 2.64211941e-08, -0.96804446)
+    },
+    -- Level 2275-2299
+    {
+        Level = {Min = 2275, Max = 2299},
+        Quest = {
+            Name = "CakeQuest2",
+            Level = 2,
+            Title = "Cake Quest 2"
+        },
+        Monster = {
+            Name = "Head Baker",
+            SpawnPoint = CFrame.new(-2216.188232421875, 82.884521484375, -12869.2939453125)
+        },
+        QuestPoint = CFrame.new(-1927.91602, 37.7981339, -12842.5391, -0.96804446, 4.22142143e-08, 0.250778586, 4.74911062e-08, 1, 1.49904711e-08, -0.250778586, 2.64211941e-08, -0.96804446)
+    },
+    -- Level 2300-2324
+    {
+        Level = {Min = 2300, Max = 2324},
+        Quest = {
+            Name = "ChocQuest1",
+            Level = 1,
+            Title = "Chocolate Quest"
+        },
+        Monster = {
+            Name = "Cocoa Warrior",
+            SpawnPoint = CFrame.new(-21.55328369140625, 80.57499694824219, -12352.3876953125)
+        },
+        QuestPoint = CFrame.new(233.22836303710938, 29.876001358032227, -12201.2333984375)
+    },
+    -- Level 2325-2349
+    {
+        Level = {Min = 2325, Max = 2349},
+        Quest = {
+            Name = "ChocQuest1",
+            Level = 2,
+            Title = "Chocolate Quest"
+        },
+        Monster = {
+            Name = "Chocolate Bar Battler",
+            SpawnPoint = CFrame.new(582.590576171875, 77.18809509277344, -12463.162109375)
+        },
+        QuestPoint = CFrame.new(233.22836303710938, 29.876001358032227, -12201.2333984375)
+    },
+    -- Level 2350-2374
+    {
+        Level = {Min = 2350, Max = 2374},
+        Quest = {
+            Name = "ChocQuest2",
+            Level = 1,
+            Title = "Chocolate Quest 2"
+        },
+        Monster = {
+            Name = "Sweet Thief",
+            SpawnPoint = CFrame.new(165.1884765625, 76.05885314941406, -12600.8369140625)
+        },
+        QuestPoint = CFrame.new(150.5066375732422, 30.693693161010742, -12774.5029296875)
+    },
+    -- Level 2375-2399
+    {
+        Level = {Min = 2375, Max = 2399},
+        Quest = {
+            Name = "ChocQuest2",
+            Level = 2,
+            Title = "Chocolate Quest 2"
+        },
+        Monster = {
+            Name = "Candy Rebel",
+            SpawnPoint = CFrame.new(134.86563110351562, 77.2476806640625, -12876.5478515625)
+        },
+        QuestPoint = CFrame.new(150.5066375732422, 30.693693161010742, -12774.5029296875)
+    },
+    -- Level 2400-2424
+    {
+        Level = {Min = 2400, Max = 2424},
+        Quest = {
+            Name = "CandyQuest1",
+            Level = 1,
+            Title = "Candy Quest"
+        },
+        Monster = {
+            Name = "Candy Pirate",
+            SpawnPoint = CFrame.new(-1310.5003662109375, 26.016523361206055, -14562.404296875)
+        },
+        QuestPoint = CFrame.new(-1150.0400390625, 20.378934860229492, -14446.3349609375)
+    },
+    -- Level 2425-2449
+    {
+        Level = {Min = 2425, Max = 2449},
+        Quest = {
+            Name = "CandyQuest1",
+            Level = 2,
+            Title = "Candy Quest"
+        },
+        Monster = {
+            Name = "Snow Demon",
+            SpawnPoint = CFrame.new(-880.2006225585938, 71.24776458740234, -14538.609375)
+        },
+        QuestPoint = CFrame.new(-1150.0400390625, 20.378934860229492, -14446.3349609375)
+    },
+    -- Level 2450-2474
+    {
+        Level = {Min = 2450, Max = 2474},
+        Quest = {
+            Name = "TikiQuest1",
+            Level = 1,
+            Title = "Tiki Quest"
+        },
+        Monster = {
+            Name = "Isle Outlaw",
+            SpawnPoint = CFrame.new(-16120.6035, 116.520554, -103.038849)
+        },
+        QuestPoint = CFrame.new(-16545.9355, 55.6863556, -173.230499)
+    },
+    -- Level 2475-2499
+    {
+        Level = {Min = 2475, Max = 2499},
+        Quest = {
+            Name = "TikiQuest1",
+            Level = 2,
+            Title = "Tiki Quest"
+        },
+        Monster = {
+            Name = "Island Boy",
+            SpawnPoint = CFrame.new(-16751.3125, 121.226219, -264.015015)
+        },
+        QuestPoint = CFrame.new(-16545.9355, 55.6863556, -173.230499)
+    },
+    -- Level 2500-2524
+    {
+        Level = {Min = 2500, Max = 2524},
+        Quest = {
+            Name = "TikiQuest2",
+            Level = 1,
+            Title = "Tiki Quest 2"
+        },
+        Monster = {
+            Name = "Sun-kissed Warrior",
+            SpawnPoint = CFrame.new(-16294.6748, 32.7874393, 1062.4856)
+        },
+        QuestPoint = CFrame.new(-16539.078125, 55.68632888793945, 1051.5738525390625)
+    },
+    -- Level 2525+
+    {
+        Level = {Min = 2525, Max = 9999},
+        Quest = {
+            Name = "TikiQuest2",
+            Level = 2,
+            Title = "Tiki Quest 2"
+        },
+        Monster = {
+            Name = "Isle Champion",
+            SpawnPoint = CFrame.new(-16933.2129, 93.3503036, 999.450989)
+        },
+        QuestPoint = CFrame.new(-16539.078125, 55.68632888793945, 1051.5738525390625)
+    }
+}
+
+-- Função para obter a quest baseada no nível do jogador
+function World3:GetQuestData(playerLevel)
+    for _, questData in ipairs(self.Quests) do
+        if playerLevel >= questData.Level.Min and playerLevel <= questData.Level.Max then
+            return questData
+        end
+    end
+    -- Retorna a última quest se o nível for maior que todas as quests disponíveis
+    return self.Quests[#self.Quests]
+end
+
+-- Função auxiliar para navegar para local da quest considerando entradas especiais
+function World3:NavigateToQuest(questData, autoFarm)
+    local target = questData.QuestPoint
+    
+    -- Verificar se a quest requer uma entrada especial e se estamos muito longe
+    if questData.RequiresSpecialEntrance and autoFarm then
+        local playerPos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+        if (target.Position - playerPos).Magnitude > 10000 then
+            -- Usar o teleporte especial para essa área
+            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("requestEntrance", questData.EntrancePoint)
+            wait(1) -- Dar tempo para o teleporte ocorrer
+        end
+    end
+    
+    return target
+end
+
+return World3
